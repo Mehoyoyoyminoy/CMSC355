63f290a749bd4d43344be9d9f91a4e5d3bdba54e
Mehoyoyoyminoy <hoangc3@vcu.edu>
2023-11-06 20:26:39 -0500

    Added main program file

---
+++ Test1.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class Test1 {
+
+    @Test
+    public void testMultiply() {
+        int result = Main.multiply(2, 2);
+        assertEquals(4, result);
+    }
+}
+
---
+++ Test2.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+
+public class Test2 {
+
+    @Test
+    public void testMultiplyWithStrings() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Main.multiply("a", "b");
+        });
+    }
+}
+
