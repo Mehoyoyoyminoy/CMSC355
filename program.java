aafa3a20b65ba142a9a3ee2101869a2f1994b4c0
Mehoyoyoyminoy <hoangc3@vcu.edu>
2023-12-15 14:32:36 -0500

    My Program

---
+++ program.java
@@ -0,0 +1,11 @@
+public class program {
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        int result = multiply(2, 2);
+        System.out.println(result);
+    }
+}
+
